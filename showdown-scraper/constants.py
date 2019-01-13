7652b3bff333d88f83494e0886e989f50605ff00
Daniel Dides <senorequeso@gmail.com>
2019-01-13 09:41:05 -0600

    [constants] Move some basic schema stuff to another file

---
+++ showdown-scraper/constants.py
@@ -0,0 +1,46 @@
+from enum import Enum
+
+class Row:
+    NUMBER = 1
+    EDITION = 2
+    NAME_TEAM = 3
+    POINTS = 4
+    YEAR = 5
+    OBC = 6
+    SPD = 7
+    POS = 8
+    H = 9
+    ICON = 10
+    CHART = 11
+    SOG = 21
+    BF = 22
+    B = 23
+    W = 24
+    S = 25
+    S_PLUS = 26
+    DB = 27
+    TR = 28
+    HR = 29
+
+PLAYER_DICT = {
+    "number": None,
+    "edition": None,
+    "name": None,
+    "team": None,
+    "points": None,
+    "year": None,
+    "obc": None,
+    "spd": None,
+    "pos": None,
+    "h": None,
+    "sog": None,
+    "bf": None,
+    "b": None,
+    "w": None,
+    "s": None,
+    "s_plus": None,
+    "db": None,
+    "tr": None,
+    "hr": None,
+}
+
